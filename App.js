c04447163818785521bca536c8672b371edc58c4
Paulo23Bunga <93455053+Paulo23Bunga@users.noreply.github.com>
2023-11-15 11:57:17 +0100

    está funcionando
    nova intercace teste

---
+++ App.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import Index from './src/telas';
+
+export default function App() {
+  return (
+    <SafeAreaView style={styles.container}>
+        <Index />
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+   // justifyContent: 'center',
+  },
+  borda:{
+    borderWidth: 3,
+    borderColor: 'red',
+    width: '100vw',
+  }
+});
